7b6763f42a273cb924ca6c783e31d650b388abc0
Brad McAlister <brad.mcalister@vitals.com>
2016-01-04 22:28:13 -0600

    updating starter

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -8,14 +8,11 @@
             'ngSanitize',
 
             /* Cross-app modules */
-            'app.loader',
-            'app.serverselect',
             'app.uibutton',
 
             /* 3rd-party modules */
             'ui.router',
             'restangular',
-            'ui.bootstrap',
             'ngStorage',
             'toastr'
         ]);
